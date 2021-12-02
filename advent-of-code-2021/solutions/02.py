4320f431e4ba1f77ff2c58364aee1ded5f89555b
Tobias Haumann <tobias.ha@gmail.com>
2021-12-02 20:15:10 +0100

    AoC 2021 day 2

---
+++ advent-of-code-2021/solutions/02.py
@@ -0,0 +1,42 @@
+import input_utils as iu
+
+def main():
+    path = '../data/02-1.txt'
+    data = iu.read_text_file_standard(path)
+    pos_keeper = {
+        'forward': 0,
+        'up': 0,
+        'down': 0
+    }
+    for line in data:
+        try:
+            d, a = line.split(' ')
+            pos_keeper[d] += int(a)
+        except Exception as e:
+            raise(e)
+    print('Part 1:', pos_keeper['forward'] * (pos_keeper['down'] - pos_keeper['up']))
+    
+    pos_keeper = {
+        'aim': 0,
+        'depth': 0,
+        'forward': 0 
+    }
+    for line in data:
+        try:
+            d, a = line.split(' ')
+            a = int(a)
+            if d == 'up':
+                pos_keeper['aim'] -= a
+            elif d == 'down':
+                pos_keeper['aim'] += a
+            else:
+                pos_keeper['forward'] += a
+                pos_keeper['depth'] += (a * pos_keeper['aim'])
+        except Exception as e:
+            raise(e)
+        print(d, a)
+        print(pos_keeper)
+    print('Part 2:', pos_keeper['forward'] * pos_keeper['depth']) 
+    
+if __name__ == '__main__':
+    main()
